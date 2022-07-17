511f5274af3c5f0c6c27a263d990f98656da4909
GMaxim80 <mgubanov80@gmail.com>
2022-07-17 18:41:18 +0300

    second commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,6 @@
 public class BonusMilesService {
     public int calculate(int price) {
-        System.out.println("Сколько миль будет начислено");
-        System.out.println((price /20) + "миль начислено");
-        return 0;
+        int miles = price / 20;
+        return miles;
     }
 }
